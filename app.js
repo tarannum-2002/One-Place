969e816b099fd48d995c7a9845787b0da5c0e11b
tarannum-2002 <tarannum.cs20@bmsce.ac.in>
2022-11-21 23:09:06 +0530

    added all the necessary files

--- app.js
+++ app.js
@@ -1,13 +1,31 @@
 const express = require('express');
 const mongoose = require('mongoose');
-const dotenv = require('dotenv').config(); 
-const app = express ();
+var methodOverride = require('method-override')
 
+const app = express();
+const Note = require('./models/note');
+const notesRouter = require('./routes/notes');
+require('dotenv').config();
 
-app.listen(process.env.PORT || 3000, () => {
-    console.log(`Server Has Started`);
+app.set('view engine', 'ejs');
+
+app.use(express.static('public'))
+app.use(express.urlencoded({ extended: false }));
+app.use(methodOverride('_method'));
+
+app.get('/', async (req, res) => {
+  const notes = await Note.find().sort('-createdAt');
+  res.render('index', { notes: notes });
 });
 
-app.get('/', (req, res) => {
-    res.send(`Yayyy! It's working`);
+//const dburi = "mongodb+srv://tara:tara0104@tarannummongodb.uf3qxwq.mongodb.net/?retryWrites=true&w=majority" ;
+mongoose.connect(process.env.SERVER, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
 });
+
+app.use('/', notesRouter);
+app.listen(process.env.PORT || 5500, () => {
+  console.log(`Server Has Started`);
+});
+
